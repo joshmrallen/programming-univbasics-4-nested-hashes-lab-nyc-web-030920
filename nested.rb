b1a2c3a9df7ff21e93277e7c65c79e205b68f4b4
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-25 05:21:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -136,5 +136,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     
+     p programmer_hash
 
 end
